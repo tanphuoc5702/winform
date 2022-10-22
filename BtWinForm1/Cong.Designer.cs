e010d646ed88a855c59e256543dcc8932623071e
tanphuoc57 <tanphuocst2307@gmail.com>
2022-10-22 21:16:40 +0700

    bt Tru

---
+++ BtWinForm1/Cong.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace BtWinForm1
+{
+    partial class Cong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.btCong = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(288, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tinh Toan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(86, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 39);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "So A";
+            // 
+            // txtA
+            // 
+            this.txtA.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtA.Location = new System.Drawing.Point(230, 119);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(255, 45);
+            this.txtA.TabIndex = 1;
+            // 
+            // btCong
+            // 
+            this.btCong.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btCong.Location = new System.Drawing.Point(521, 317);
+            this.btCong.Name = "btCong";
+            this.btCong.Size = new System.Drawing.Size(165, 71);
+            this.btCong.TabIndex = 2;
+            this.btCong.Text = "+";
+            this.btCong.UseVisualStyleBackColor = true;
+            this.btCong.Click += new System.EventHandler(this.btCong_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(87, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 39);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "KQ";
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtKQ.Location = new System.Drawing.Point(231, 246);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(255, 45);
+            this.txtKQ.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(86, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 39);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "So B";
+            // 
+            // txtB
+            // 
+            this.txtB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtB.Location = new System.Drawing.Point(230, 183);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(255, 45);
+            this.txtB.TabIndex = 1;
+            // 
+            // Cong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btCong);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Cong";
+            this.Text = "Cong";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Button btCong;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtB;
+    }
+}
